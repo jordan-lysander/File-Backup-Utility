f4c0fd572770fd3f19e5ad46b0bb1f8975659cb1
Jordan Lysander <jordan.l.higgs@gmail.com>
2025-10-20 14:14:36 +0100

    Fully implemented --quiet and --dry flags for better control Add simple global log function to account for quiet flag

--- backup-utility.py
+++ backup-utility.py
@@ -5,16 +5,27 @@ import argparse
 parser = argparse.ArgumentParser(description="copy files from one directory to another")
 parser.add_argument("src", nargs="?", help="the source directory to copy from")
 parser.add_argument("dst", nargs="?", help="the destination directory to copy to")
-parser.add_argument("-d", "--dry", action="store_true", help="will print actions to console instead of performing them")
-parser.add_argument("-q", "--quiet", action="store_true", help="disables progress console printing")
+options = parser.add_mutually_exclusive_group()
+options.add_argument("-d", "--dry", action="store_true", help="will print actions to console instead of performing them")
+options.add_argument("-q", "--quiet", action="store_true", help="disables progress console printing")
+
+# simple global log function to respect the '--quiet' argument
+log = lambda *args, **kwargs: print(*args, **kwargs)
+
+def create_path(path: str) -> Path:
+    p = Path(path)
+    return p if p.is_absolute() else Path.cwd() / p
 
 # copy the source directory, walking from top to bottom
-def copy_files(src_dir: Path, dst_dir: Path, dry=False, quiet=False):
+def copy_files(src_dir: Path, dst_dir: Path, dry=False) -> None:
     # create the destination directory if it doesn't already exist
-    dst_dir.mkdir(parents=True, exist_ok=True)
+    if dry:
+        log(f"[DRY RUN] Would create directory: {dst_dir}")
+    else:
+        dst_dir.mkdir(parents=True, exist_ok=True)
 
     # loop through the source directory
-    print("- Scanning source directory...")
+    log("- Scanning source directory...")
 
     for dirpath, dirnames, filenames in src_dir.walk():
         # get the path to the target directory relative to any subdirectories
@@ -23,56 +34,65 @@ def copy_files(src_dir: Path, dst_dir: Path, dry=False, quiet=False):
         # create all subdirectories in the target directory
         for dirname in dirnames:
             dst_path = dst_dir / relative_dir_path / dirname
-            dst_path.mkdir(exist_ok=True)
-            print("-- Directory copied:", dirname)
+            if dry:
+                log(f"[DRY RUN] -- Dir copied: {dst_path}")
+            else:
+                dst_path.mkdir(exist_ok=True)
+                log(f"-- Dir copied: {dirname}")
 
         # copy all files to the target directory
         for filename in filenames:
             src_path = dirpath / filename
             dst_path = dst_dir / relative_dir_path / filename
 
-            # skip unmodified files
+            # skip unmodified files and account for the '--dry' flag
             if dst_path.exists() and dst_path.stat().st_mtime >= src_path.stat().st_mtime:
-                print("-~- File not copied:", filename, "(unmodified)")
+                if dry:
+                    log(f"[DRY RUN] ->- Skipped: {filename}")
+                else:
+                    log(f"->- Skipped: {filename}")
                 continue
+            elif dst_path.exists():
+                if dry:
+                    log(f"[DRY RUN] -+- Updated: {filename}")
+                else:
+                    log(f"-+- Updated: {filename}")
+                continue
+            else:
+                if dry:
+                    log(f"[DRY RUN] --- Copied:  {filename}")
+                else:
+                    shutil.copy2(src_path, dst_path)
+                    log(f"--- Copied:  {filename}")
 
-            shutil.copy2(src_path, dst_path)
-            print("--- File copied:", filename)
-
-def instructions():
+def instructions() -> str:
     return """--- Backup Utility ---
 This basic CLI application copies files from one directory to another,
 attempting to preserve metadata and ignoring unmodified files.
 
 Paths entered below will be relative to your current directory, unless using absolute paths."""
 
-def get_src_and_dst():
+def get_src_and_dst() -> tuple[Path, Path]:      # return the source and destination paths
     print(instructions())
 
-    src = Path(input("- Source: "))
-    if not src.is_absolute():
-        print("Source path not absolute")
-        src = Path.cwd() / src
-    print("Source path:", src)
+    src = create_path(input("- Source: "))
+    log("Source path:", src)
 
-    dst = Path(input("- Destination: "))
-    if not dst.is_absolute():
-        print("Destination path not absolute")
-        dst = Path.cwd() / dst
-    print("Destination path:", dst)
+    dst = create_path(input("- Destination: "))
+    log("Destination path:", dst)
 
     return src, dst
         
 if __name__ == "__main__":
     args = parser.parse_args()
+    
+    if args.quiet:  # do not print logs if --quiet is flagged as true
+        log = lambda *args, **kwargs: None
+
     if args.src and args.dst:
-        src = Path(args.src)
-        if not src.is_absolute():
-            src = Path.cwd() / src
-        dst = Path(args.dst)
-        if not dst.is_absolute():
-            dst = Path.cwd() / dst
-        copy_files(src, dst)
+        src = create_path(args.src)
+        dst = create_path(args.dst)
+        copy_files(src, dst, dry=args.dry)
     else:
         src, dst = get_src_and_dst()
-        copy_files(src, dst)
+        copy_files(src, dst, dry=args.dry)
